fc4070fd6a016b87174c62c3793f2fac015b1417
emejiasc85 <emejiasc85@gmail.com>
2014-09-24 23:54:52 -0600

    medias queries para el header y main menu, cambios sutiles en los estilos y semantica y estilos de breadcrumbs y shortcuts

--- static/js/dashboard.js
+++ static/js/dashboard.js
@@ -1,11 +1,35 @@
 var $avatar 		= $('#submenu-avatar'),
-	$subMenuAvatar	= $('.HeaderNav-subList')
+	$subMenuAvatar 	= $('.HeaderNav-subList'),
+	$menuItem 		= $('.MainMenu-item'),
+	$MainMenu 		= $('.MainMenu'),
+	$dropMainMenu	= $('.Header-DropBottom')
 
 
 //mostramos el formulario de denucias
 function toggleSubMenu() {
-	$subMenuAvatar.slideToggle()
+	$subMenuAvatar.slideToggle('fast')
 	return false
 }
 
+function toggleMainMenu() {
+	$MainMenu.slideToggle('fast', 'swing')
+	return false
+}
+
+
+
+//mostramos los submenus del menu principal
+function viewSubMenu(){
+	 $(this).children('.MainMenu-subList').toggle('fast', 'swing');
+}
+
+
+$('.MainMenu-subList').on('click', function (event) {
+    event.stopPropagation();
+});
+
+$menuItem.on('click', viewSubMenu)
 $avatar.on('click', toggleSubMenu)
+$dropMainMenu.on('click', toggleMainMenu)
+
+
